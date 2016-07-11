4bf972a4dae7614888d7cb7783ae174ed742f1bf
km00700 <km00700@gmail.com>
2016-07-11 08:02:43 -0700

    Initial Commit

---
+++ Keypad Piano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keypad Piano
+//
+//  Created by Kai Mills on 7/11/16.
+//  Copyright © 2016 Kai Mills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
